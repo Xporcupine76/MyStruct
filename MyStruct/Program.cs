7280d7af2ab1c79b301a9c2622ba8b2285e03c44
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:07:18 +0200

    Add project files.

---
+++ MyStruct/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Channels;
+namespace MyStruct
+{
+
+    class Peogram
+    {
+        static void Main(string[] args)
+        {
+
+            // Mark mark = new Mark();
+            //mark.DoThis();
+            Mark m2;
+            m2.x = 6;
+            m2.y = 7;
+            m2.DoThis();
+
+        }
+    }
+    public struct Mark
+    {
+
+        public int x = 6;
+        public int y = 7;
+        
+        public Mark(/*int a, int y*/)
+        {
+          // x = a;
+          // y = b;  
+        }
+        
+        public void DoThis()
+        {
+
+            Console.WriteLine(x + y);
+
+        }
+
+    }
+}
